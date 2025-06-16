d709f1d53a2b0fd447cf78b9ffca3d205df3b208
Kerop1981 <vova.abraamyan@mail.ru>
2025-06-16 18:12:15 +0300

    сделал

--- src/app/models/todo.model.ts
+++ src/app/models/todo.model.ts
@@ -1,6 +1,10 @@
+ type TodoStatus = 'active'| 'completed'|'archived'
 
 export interface TodoItem {
     id:string;
     title:string;
     completed:boolean;
+    status: TodoStatus;
+    createdAt?: string;
+    dueDate?: string;
 }
