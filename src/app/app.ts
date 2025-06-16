c5eabb1078bc1d9a2adcb6c08b023cae45dcd5c6
Kerop1981 <vova.abraamyan@mail.ru>
2025-06-16 20:36:53 +0300

    feat(todo): добавлен компонент отображения и добавления задач

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoComponent } from "./components/todo-component/todo-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TodoComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
